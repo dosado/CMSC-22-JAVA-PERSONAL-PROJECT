30600b49dee9b8adedeaff87ba856896a83d61aa
dosado <77151902+dosado@users.noreply.github.com>
2021-01-10 22:32:30 +0800

    Add files via upload
    The "Fitnesstesting.java" file is my Project Source Code. The other Java Files are some of my test codes for the TDD. Included in the Upload is the Executable JAR File for the Javadoc Output.

---
+++ src/testing/AllTests.java
@@ -0,0 +1,11 @@
+package testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({Sets_Sex_Test.class, AccountnameTest.class})
+public class AllTests {
+
+}
---
+++ src/testing/BMI_result_Kidstest.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BMI_result_Kidstest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
